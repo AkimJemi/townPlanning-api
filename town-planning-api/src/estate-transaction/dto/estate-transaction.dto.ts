894346e32aa1c3d97e176cfcb080608c21e87b31
AkimJemi <rlawoals623@gmail.com>
2024-11-20 21:30:36 +0900

    [0.0.7] 取引価格、クエリパラメーターのバリデーション

--- town-planning-api/src/estate-transaction/dto/estate-transaction.dto.ts
+++ town-planning-api/src/estate-transaction/dto/estate-transaction.dto.ts
@@ -1,10 +1,29 @@
+import { Type } from 'class-transformer';
+import { IsIn, IsInt, IsNotEmpty, IsOptional, Max, Min } from 'class-validator';
+
 export class EstateTransactionDto {
+
+  @IsNotEmpty()
+  @IsInt()
+  @Type(() => Number)
+  @Min(2015)
+  @Max(2018)
   year: number;
 
+  @IsNotEmpty()
+  @IsInt()
+  @Type(() => Number)
+  @Min(8)
+  @Max(14)
   prefCode: number;
 
+  @IsOptional()
   cityCode: number;
 
+  @IsNotEmpty()
+  @IsInt()
+  @Type(() => Number)
+  @IsIn([1, 2])
   displayType: number;
 }
 export class EstateTransactionResponseDto {
