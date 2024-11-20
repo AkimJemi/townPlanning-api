57093431c359a7e75eb271be7d17efecafdfa3db
AkimJemi <rlawoals623@gmail.com>
2024-11-20 21:56:59 +0900

    [0.0.8] ➀実行手順作成、②バージョン管理用Swagger設定

--- town-planning-api/src/main.ts
+++ town-planning-api/src/main.ts
@@ -4,11 +4,22 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(process.env.PREFIX);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('不動産取引価格を検索するAPI')
+    .setDescription('NestJS 不動産取引価格を検索するAPI')
+    .setVersion('0.0.8')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
